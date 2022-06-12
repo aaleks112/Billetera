88f358742c2022ba846e734832eee9e98ef17088
Agustin Aleksandroff <agustinaleks@gmail.com>
2022-06-12 00:02:09 -0300

    Add project files.

---
+++ Billetera/Modelo/Billetera.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetes.Modelo
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+ 
+       
+        public Billetera(int billetes10, int billetes20, int billetes50, int billetes100, int billetes200, int billetes500, int billetes1000)
+        {
+            BilletesDe10 = billetes10;
+            BilletesDe20 = billetes20;
+            BilletesDe50 = billetes50;
+            BilletesDe100 = billetes100;
+            BilletesDe200 = billetes200;
+            BilletesDe500 = billetes500;
+            BilletesDe1000 = billetes1000;
+        }
+
+      
+        public decimal Total(int BilletesDe10, int BilletesDe20, int BilletesDe50, int BilletesDe100, int BilletesDe200, int BilletesDe500, int BilletesDe1000)
+        {
+            int de10 = BilletesDe10 * 10;
+            int de20 = BilletesDe20 * 20;
+            int de50 = BilletesDe50 * 50;
+            int de100 = BilletesDe100 * 100;
+            int de200 = BilletesDe200 * 200;
+            int de500 = BilletesDe500 * 500;
+            int de1000 = BilletesDe1000 * 1000;
+
+           int importeTotal = de10 + de20 + de50 + de100 + de200 + de500 + de1000;
+            return importeTotal;
+        }
+
+
+        public Billetera Combinar(Billetera billeteraAdicional)
+        {
+            int sumade10 = (BilletesDe10 + billeteraAdicional.BilletesDe10) * 10;
+            int sumade20 = (BilletesDe20 + billeteraAdicional.BilletesDe20) * 20;
+            int sumade50 = (BilletesDe50 + billeteraAdicional.BilletesDe50) * 50;
+            int sumade100 = (BilletesDe100 + billeteraAdicional.BilletesDe100) * 100;
+            int sumade200 = (BilletesDe200 + billeteraAdicional.BilletesDe200) * 200;
+            int sumade500 = (BilletesDe500 + billeteraAdicional.BilletesDe500) * 500;
+            int sumade1000 = (BilletesDe1000 + billeteraAdicional.BilletesDe1000) * 1000;
+
+            Billetera nuevaBilletera = new Billetera(sumade10,sumade20,sumade50,sumade100,sumade200,sumade500,sumade1000);
+
+            return nuevaBilletera;
+        }
+
+
+        public void BilleteraAVaciar(Billetera billeteraAVaciar)
+        {
+            billeteraAVaciar.BilletesDe10 = 0;
+            billeteraAVaciar.BilletesDe20 = 0;
+            billeteraAVaciar.BilletesDe50 = 0;
+            billeteraAVaciar.BilletesDe100 = 0;
+            billeteraAVaciar.BilletesDe200 = 0;
+            billeteraAVaciar.BilletesDe500 = 0;
+            billeteraAVaciar.BilletesDe1000 = 0;
+        }
+    }
+}
+
---
+++ Billetera/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+BilletesDe10
+BilletesDe20
+BilletesDe50
+BilletesDe100
+BilletesDe200
+BilletesDe500
+BilletesDe1000
+
+Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera teniendo en cuenta
+la cantidad de billetes que se tenga de cada tipo.
+
+Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva Billetera 
+con la suma o combinacion del dinero de ambas billeteras.  
+Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+*/
+
+using Billetes.Modelo;
+
+// cargo cantidad de billetes x billetera
+var billetera1 = new Billetera(10,5,2,1,1,1,2);
+var billetera2 = new Billetera(10, 5, 2, 1, 1, 1, 4);
+
+
+
+Console.WriteLine("El importe total de la billetera es: " +
+    billetera1.Total(billetera1.BilletesDe10,
+                   billetera1.BilletesDe20,
+                   billetera1.BilletesDe50,
+                   billetera1.BilletesDe100,
+                   billetera1.BilletesDe200,
+                   billetera1.BilletesDe500,
+                   billetera1.BilletesDe1000));
+
+
+Billetera b3 = billetera1.Combinar(billetera2);
+
+// para corroborar cantidad de billetes
+Console.WriteLine($"La suma de dinero en billetes de 10 es ${b3.BilletesDe10} de 20 ${b3.BilletesDe20} de 50 ${b3.BilletesDe50}" +
+    $"de 100 ${b3.BilletesDe100} de 500 ${b3.BilletesDe500} de 1000 $ {b3.BilletesDe1000}"); 
+
+// llamo método para vaciar billeteras
+billetera1.BilleteraAVaciar(billetera1);
+billetera2.BilleteraAVaciar(billetera2);
+
+Console.WriteLine();
+// para corroborar billetera vacía
+Console.WriteLine($"la cantidad de billetes de 10 de la billetera 1 después de borrada es: {billetera1.BilletesDe10}");
+Console.WriteLine($"la cantidad de billetes de 20 de la billetera 2 después de borrada es: {billetera2.BilletesDe20} etc etc");
